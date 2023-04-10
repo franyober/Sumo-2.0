a74bb87afeefa2e5254183bd496ba32aa59273b8
franyober <franyober123@hotmail.com>
2023-04-10 09:17:56 -0500

    corrigiendo error de variables extern

--- include/puenteH.h
+++ include/puenteH.h
@@ -1,10 +1,10 @@
- extern int enA=3;  //Pin para controlar la velocidad
- extern int in1=4;  //Terminal A del motor derecho
- extern int in2=2; //terminal в del motor derecho
+ extern int enA;  //Pin para controlar la velocidad
+ extern int in1;  //Terminal A del motor derecho
+ extern int in2; //terminal в del motor derecho
 
- extern int enB =6; //Pin para controlar la velocidad
- extern int in3=5;  //terminal A del motor izquierdo
- extern int in4=7; //terminal в del motor izquierdo
+ extern int enB; //Pin para controlar la velocidad
+ extern int in3;  //terminal A del motor izquierdo
+ extern int in4; //terminal в del motor izquierdo
 
 
 void adelante();
